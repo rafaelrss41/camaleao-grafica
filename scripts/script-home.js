a3c564769bc20e2dc9e39e912a133c5ac48aa838
rafaelrss41 <rss41@hotmail.com>
2024-05-14 18:31:02 -0300

    grafica

--- scripts/script-home.js
+++ scripts/script-home.js
@@ -8,7 +8,7 @@ let hill5=  document.getElementById('hill5');
 window.addEventListener('scroll', () =>{
     let value = window.scrollY;
 
-    text.style.marginTop =value * 1.5 + 'px';
+    text.style.marginTop =value * 1.7 + 'px';
     leaf.style.top =value * -1.5 + 'px';
     leaf.style.left =value * 1.2 + 'px';
     hill5.style.left =value * 1.2 + 'px';
